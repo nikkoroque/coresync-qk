4d87d6e9b7d06f03b9d4ae1850d342913ba5da97
Nikko Roque <106610608+nikkoroque@users.noreply.github.com>
2024-12-30 23:45:30 -0800

    created section code backend logic and endpoints
    created section code backend logic and endpoints

--- src/main/java/org/coresync/app/model/SectionCode.java
+++ src/main/java/org/coresync/app/model/SectionCode.java
@@ -2,7 +2,7 @@ package org.coresync.app.model;
 
 import jakarta.persistence.*;
 
-import java.sql.Timestamp;
+import java.time.OffsetDateTime;
 
 @Entity
 @Table(name = "section_code", schema = "inventory_mgt", catalog = "coresync")
@@ -19,16 +19,31 @@ public class SectionCode {
     private String description;
     @Basic
     @Column(name = "creation_date")
-    private Timestamp creationDate;
+    private OffsetDateTime creationDate;
     @Basic
     @Column(name = "created_by_user")
     private String createdByUser;
     @Basic
     @Column(name = "last_update_date")
-    private Timestamp lastUpdateDate;
+    private OffsetDateTime lastUpdateDate;
     @Basic
     @Column(name = "last_updated_by_user")
     private String lastUpdatedByUser;
+
+    public SectionCode() {
+
+    }
+
+    public SectionCode(int id, String code, String description, OffsetDateTime creationDate, String createdByUser,
+                       OffsetDateTime lastUpdateDate, String lastUpdatedByUser) {
+        this.id = id;
+        this.code = code;
+        this.description = description;
+        this.creationDate = creationDate;
+        this.createdByUser = createdByUser;
+        this.lastUpdateDate = lastUpdateDate;
+        this.lastUpdatedByUser = lastUpdatedByUser;
+    }
 
     public int getId() {
         return id;
@@ -54,11 +69,11 @@ public class SectionCode {
         this.description = description;
     }
 
-    public Timestamp getCreationDate() {
+    public OffsetDateTime getCreationDate() {
         return creationDate;
     }
 
-    public void setCreationDate(Timestamp creationDate) {
+    public void setCreationDate(OffsetDateTime creationDate) {
         this.creationDate = creationDate;
     }
 
@@ -70,11 +85,11 @@ public class SectionCode {
         this.createdByUser = createdByUser;
     }
 
-    public Timestamp getLastUpdateDate() {
+    public OffsetDateTime getLastUpdateDate() {
         return lastUpdateDate;
     }
 
-    public void setLastUpdateDate(Timestamp lastUpdateDate) {
+    public void setLastUpdateDate(OffsetDateTime lastUpdateDate) {
         this.lastUpdateDate = lastUpdateDate;
     }
 
